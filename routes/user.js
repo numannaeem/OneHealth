9510c80e5e9e01fdf4997658a90aaecb01db6d63
Prayag Sharma <85249351+prg2308@users.noreply.github.com>
2021-12-25 13:31:49 +0530

    Refactored User routes

--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,6 @@ const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const userControllers = require('../controllers/user')
 
-router.post('/login', passport.authenticate('local'), userControllers.login)
+router.post('/login', passport.authenticate('local'), catchAsync(userControllers.login))
 
 module.exports = router;
